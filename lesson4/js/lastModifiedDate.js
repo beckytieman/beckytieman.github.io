27b1ae24cbcf0968334e00e6b880323c96c2edbf
beckytieman <beckytieman@gmail.com>
2020-01-29 13:05:34 -0700

    crazy

--- lesson4/js/lastModifiedDate.js
+++ lesson4/js/lastModifiedDate.js
@@ -1,2 +1,10 @@
-var date = document.lastModified;
-document.write("Last Updated: "+date);
+var d=new Date()
+    var weekday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday",
+                    "Friday","Saturday")
+    var monthname=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug",
+                    "Sep","Oct","Nov","Dec")
+    document.write(weekday[d.getDay()] + " ")
+    document.write(d.getDate() + ", ")
+    document.write(monthname[d.getMonth()] + " ")
+    document.write(d.getFullYear())
+    var p = document.getElementById("day").innerHTML = d;
