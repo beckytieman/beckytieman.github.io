59bd9bf2e224abe7d2d62688d31f619cc60e451f
beckytieman <beckytieman@gmail.com>
2020-02-19 05:44:25 -0700

    first

---
+++ Lesson7/js/webFont.js
@@ -0,0 +1,5 @@
+WebFont.load({
+    google: {
+      families: ["Francois One", "Montserrat"]
+    }
+  });
---
+++ Lesson7/js/windchill.js
@@ -0,0 +1,18 @@
+
+var temp = parseFloat(document.getElementById ('Current').textContent);
+var speed = parseFloat(document.getElementById ('WindSpeed').textContent);
+var output = Math.round(windChill(temp, speed))
+
+if(temp <= 50 && speed > 3) {
+    document.getElementById('WindChill').innerHTML = output + "&deg;F"
+}
+else {
+    document.getElementById('WindChill').innerHTML = "N/A"
+}
+
+
+//calculation
+
+function windChill(tempF, speed) {
+return 35.74 + (0.6215 * tempF) - (35.75*(speed**0.16)) + (0.4275*(tempF)*(speed**0.16));
+}
