98108e3574b4f5c6b0646cdc267ead256739a5a5
beckytieman <beckytieman@gmail.com>
2020-03-13 15:41:18 -0700

    k

--- lesson9/js/towns.js
+++ lesson9/js/towns.js
@@ -10,7 +10,7 @@ fetch(requestURL)
     
 
     for (let i = 0; i < towns.length; i++ ) {
-        if (towns[i].name == 'Preston' || towns[i].name == 'Soda Springs' || towns[i].name == 'Fish Haven') {
+        if (towns[i].name == 'Preston') {
         let townsDiv = document.createElement('div');
         let card = document.createElement('section');
         
@@ -49,4 +49,86 @@ fetch(requestURL)
 
 
     }
+    for (let i = 0; i < towns.length; i++ ) {
+      if (towns[i].name == 'Soda Springs') {
+      let townsDiv = document.createElement('div');
+      let card = document.createElement('section');
+      
+
+      let h2 = document.createElement('h2');
+      let h3 = document.createElement('h3');
+      let year = document.createElement('p')
+      let pop = document.createElement('p')
+      let rain = document.createElement('p')
+      let image = document.createElement('img');
+
+      h2.textContent = towns[i].name;
+      h3.textContent = towns[i].motto;
+      year.textContent = "Year Founded: " + towns[i].yearFounded;
+      pop.textContent = "Population: " + towns[i].currentPopulation;
+      rain.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
+      image.setAttribute('src', "images/" + towns[i].photo);
+      image.setAttribute('alt', towns[i].name);
+
+      card.appendChild(h2);
+      card.appendChild(h3);
+      card.appendChild(year);
+      card.appendChild(pop);
+      card.appendChild(rain);
+      
+      townsDiv.appendChild(card);
+      townsDiv.appendChild(image);
+    
+
+      document.querySelector('div.towns').appendChild(townsDiv);
+      
+  }
+      else {
+          continue;
+      }
+
+
+  }
+
+  for (let i = 0; i < towns.length; i++ ) {
+    if (towns[i].name == 'Fish Haven') {
+    let townsDiv = document.createElement('div');
+    let card = document.createElement('section');
+    
+
+    let h2 = document.createElement('h2');
+    let h3 = document.createElement('h3');
+    let year = document.createElement('p')
+    let pop = document.createElement('p')
+    let rain = document.createElement('p')
+    let image = document.createElement('img');
+
+    h2.textContent = towns[i].name;
+    h3.textContent = towns[i].motto;
+    year.textContent = "Year Founded: " + towns[i].yearFounded;
+    pop.textContent = "Population: " + towns[i].currentPopulation;
+    rain.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
+    image.setAttribute('src', "images/" + towns[i].photo);
+    image.setAttribute('alt', towns[i].name);
+
+    card.appendChild(h2);
+    card.appendChild(h3);
+    card.appendChild(year);
+    card.appendChild(pop);
+    card.appendChild(rain);
+    
+    townsDiv.appendChild(card);
+    townsDiv.appendChild(image);
+  
+
+    document.querySelector('div.towns').appendChild(townsDiv);
+    
+}
+    else {
+        continue;
+    }
+
+
+}
+
   });
